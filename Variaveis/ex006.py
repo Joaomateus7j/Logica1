45c0ec26f445bafe85ebba2365797479c3efe6df
João Mateus <mjoaomateus10@gmail.com>
2026-05-06 17:46:24 -0300

    Exercício concluido

---
+++ Variaveis/ex006.py
@@ -0,0 +1,10 @@
+'''
+Exercício 1: O Gerador de Crachá (RH)
+Esse exercício serve para você praticar como juntar informações de diferentes variáveis em uma única frase (o que chamamos de concatenação).
+'''
+
+nome = 'joao'
+sobrenome = 'Santos'
+cargo = 'Desenvolvedor Back-end'
+nascimento = '2006'
+print(f'{nome}|{sobrenome}|Cargo:|{cargo}|Ano:{nascimento}')
