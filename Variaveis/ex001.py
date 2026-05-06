a9ca2b32322e2a3e23508f564279e44067b1b1ac
João Mateus <mjoaomateus10@gmail.com>
2026-05-05 21:38:43 -0300

    Carregamento de arquivos

---
+++ Variaveis/ex001.py
@@ -0,0 +1,15 @@
+# Variáveis
+# Nome, celular, endereço...
+
+velocidade_internet = 400
+print(velocidade_internet)
+# Número inteiros(int)
+idade = 15  
+# Numero decimais
+nota = 8.5
+#Textos(string)
+nome_completo = 'Joao mateus'
+#Booleanos(True e False)
+pode_entrar = True
+
+print(type(pode_entrar))
