543d1ffdc62d72f0db477941373810b1995a2817
João Mateus <mjoaomateus10@gmail.com>
2026-05-07 13:40:09 -0300

    Mais 2 exercícios

---
+++ Aplicando/ex005.py
@@ -0,0 +1,14 @@
+'''
+Exercício 5: Verificar Maior de Idade ⭐ (Mais fácil)
+
+'''
+
+
+idade = int(input('Poderia falar qual e sua idade? '))
+
+maior = 18
+
+if idade >= maior:
+    print('Você e de maior')
+elif idade < maior:
+    print('Você possui menos de 18 anos')
---
+++ Aplicando/ex006.py
@@ -0,0 +1,11 @@
+# Exercício 6: Aprovado ou Reprovado ⭐⭐ (Médio)
+
+aluno = float(input('Poderia me falar qual e sua nota? '))
+
+nota1 = 7
+nota2 = 6 
+
+if aluno >= nota1:
+    print('Aprovado')
+elif aluno <= nota2:
+    print('Reprovado')
